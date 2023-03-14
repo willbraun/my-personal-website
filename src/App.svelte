67f539c9ccc4ed8c87722df86a4071ab50e9ca4a
willbraun <williamhbraun1@gmail.com>
2023-03-14 08:01:03 -0400

    added vite/svelte placeholder site

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<main>
+	<div class="content">
+		<h1>Will Braun</h1>
+		<p>This site is in progress!</p>
+	</div>
+</main>
+
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	.content {
+		text-align: center;
+	}
+</style>
